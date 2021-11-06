2517a9052cd75f4aa2063f87a6d050dfa2df8f73
i13302 <s20g470@stu.kagawa-u.ac.jp>
2021-11-06 15:36:14 +0900

    onelinerは展開が難しい

--- pandoc/docker-entrypoint.sh
+++ pandoc/docker-entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-echo $@
-/bin/sh -c "$@"
+
+#--
+echo $1 # pandoc
+echo $2 # markdown dir
+echo $3 # html dir
+echo $4 # css dir
+#--
+
+cd $2
+ls *.md | xargs -I{} echo "$1 {} $(ls ../$4/*.css | xargs -I{} echo "-c {}") -f markdown --self-contained -o ../'$3'/{}"
+
+
+# /bin/sh -c "$@"
