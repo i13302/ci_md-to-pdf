4e06cec583df583e7fde68e5168f092a1ebf354c
i13302 <s20g470@stu.kagawa-u.ac.jp>
2021-11-06 16:45:24 +0900

    perlはtestのみに

--- check.pl
+++ check.pl
@@ -8,19 +8,19 @@ use Data::Dumper;
 use Getopt::Long 'GetOptions';
 use File::Basename 'basename';
 
-sub ret_chomp
-{
-	my $var=shift;
-	chomp($var);
-	
-	return $var
-}
-
 sub ret_ext_change
 {
 	my ($dstfile,$toext)=@_;
 	$dstfile=~s/^(.*)\..*$/$1/;
 	return $dstfile.'.'.$toext;
+}
+
+sub files_ext_change
+{
+	my ($ref_files,$toext)=@_;
+	foreach(@$ref_files){
+		$_=&ret_ext_change($_,$toext)
+	}
 }
 
 sub trim_dirname
@@ -31,81 +31,23 @@ sub trim_dirname
 	}
 }
 
-sub trim_workdir_css_files
-{
-	my ($work_dir,$ref_css_path)=@_;
-	foreach(@$ref_css_path){
-		$_=~s/^$work_dir\///;
-	}
-}
-
-sub docker_cmd_mdtohtml
-{
-	my ($work_dir,$md_dir,$html_dir,$mdtohtml,$pwd,$md_name,$html_name,$ref_css_path)=@_;
-	
-	my @cmd=();
-	$cmd[0]='docker run --rm --volume "'.$pwd.'/'.$work_dir.':/data" '.$mdtohtml.' \'cd '.$md_dir.'; pandoc -f markdown --self-contained '.$md_name;
-	
-	$cmd[1]='';
-	foreach (@$ref_css_path){
-		$cmd[1].='-c ../'.$_.' ';
-	}
-	
-	$cmd[2]='-o ../'.$html_dir.'/'.$html_name.'\'';
-	
-	return join(' ',@cmd);
-}
-
-sub docker_cmd_htmltopdf
-{
-	my ($work_dir,$html_dir,$pdf_dir,$htmltopdf,$pwd,$html_name,$pdf_name,$tz)=@_;
-	my $cmd='docker run --rm -e TZ='.$tz.' --volume "'.$pwd.'/'.$work_dir.':/data" '.$htmltopdf.' "'.$html_dir."/".$html_name.' '.$pdf_dir.'/'.$pdf_name.'"';
-	
-	return $cmd;
-}
-
 sub main
 {
 	my $work_dir='work';
 	my $md_dir  ='markdown';
-	my $html_dir='html';
-	my $css_dir ='css';
 	my $pdf_dir ='pdf';
-	my $mdtohtml ='i13302/pandoc';
-	my $htmltopdf='i13302/printout';
-	my $pwd      =&ret_chomp(`pwd`);
-	my $tz       ='Asia/Tokyo';
 
 	GetOptions(
 		'work=s'     =>\$work_dir,
 		'markdown=s' =>\$md_dir,
-		'html=s'     =>\$html_dir,
-		'css=s'      =>\$css_dir,
 		'pdf=s'      =>\$pdf_dir,
-		'mdtohtml=s' =>\$mdtohtml ,
-		'htmltopdf=s'=>\$htmltopdf,
-		'pwd=s'      =>\$pwd,
-		'TZ=s'       =>\$tz
 	);
 
 	my @md_path=glob $work_dir.'/'.$md_dir.'/*.md';
-	my @css_path=glob $work_dir.'/'.$css_dir.'/*.css';
+	trim_dirname(\@md_path);
 	
-	trim_workdir_css_files($work_dir,\@css_path);
-
-	system('cp '.$work_dir.'/base/* '.$work_dir.'/pdf/');
-
-	my @pdf_path=();
-	foreach(@md_path){
-		my $md_name=&basename($_);
-		my $html_name=&ret_ext_change($md_name,'html');
-		my $pdf_name=&ret_ext_change($md_name,'pdf');
-		push(@pdf_path,$pdf_name);
-		
-		my @cmd=(&docker_cmd_mdtohtml($work_dir,$md_dir,$html_dir,$mdtohtml,$pwd,$md_name,$html_name,\@css_path) , &docker_cmd_htmltopdf($work_dir,$html_dir,$pdf_dir,$htmltopdf,$pwd,$html_name,$pdf_name,$tz));
-		print Dumper @cmd;
-		system(join('&&',@cmd));
-	}
+	my @pdf_path=@md_path;
+	files_ext_change(\@pdf_path,'pdf');
 	
 	my @builed_pdf_path=glob $work_dir.'/'.$pdf_dir.'/*.pdf';
 	trim_dirname(\@builed_pdf_path);
